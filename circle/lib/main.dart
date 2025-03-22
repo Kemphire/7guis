4423db3196999046d8db4db2849e6f83b3fdea33
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-03-22 13:05:21 +0530

    implemented till undo functionality

--- circle/lib/main.dart
+++ circle/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:circle/stack.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,116 +9,208 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Circle Drawer",
+      home: MyCanvas(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyCanvas extends StatefulWidget {
+  const MyCanvas({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyCanvas> createState() => _MyCanvasState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyCanvasState extends State<MyCanvas> {
+  List<Circle> activeCircles = [];
+  StackDs<List<Circle>> stackOfChanges = StackDs<List<Circle>>();
 
-  void _incrementCounter() {
+  bool showUndo() => !stackOfChanges.isEmpty;
+
+  void updateStack() {
+    var deepCopy = activeCircles.map((circle) => circle.copy()).toList();
+    if (stackOfChanges.isEmpty || !listEquals(stackOfChanges.peek, deepCopy)) {
+      stackOfChanges.push(deepCopy);
+    }
+  }
+
+  final canvasKey = GlobalKey();
+
+  void _addCircles(
+    TapDownDetails details,
+    double screenWidth,
+    double screenHeight,
+  ) {
+    // using our custom key to get the context of our canvas
+    RenderBox box = canvasKey.currentContext!.findRenderObject() as RenderBox;
+    Offset localPosition = box.globalToLocal(details.globalPosition);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      activeCircles.add(
+        Circle(
+          x: localPosition.dx / screenWidth,
+          y: localPosition.dy / screenHeight,
+          radius: 30,
+          showDialog: false,
+          color: Colors.grey,
+        ),
+      );
+      updateStack();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var widthOfScreen = MediaQuery.of(context).size.width;
+    var heightOfScreen = MediaQuery.of(context).size.height;
+    var widthOfCanvas = widthOfScreen / 2;
+    var heightOfCanvas = heightOfScreen / 2;
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text("Draw Cricles")),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            ElevatedButton(
+              onPressed:
+                  showUndo()
+                      ? () {
+                        setState(() {
+                          stackOfChanges.pop();
+
+                          if (stackOfChanges.isEmpty) {
+                            activeCircles = [];
+                          } else {
+                            activeCircles =
+                                stackOfChanges.peek
+                                    .map((c) => c.copy())
+                                    .toList();
+                          }
+                        });
+                      }
+                      : null,
+              child: Text("Undo"),
+            ),
+            GestureDetector(
+              onTapDown:
+                  (details) =>
+                      _addCircles(details, widthOfCanvas, heightOfCanvas),
+              child: Container(
+                key: canvasKey,
+                width: widthOfCanvas,
+                height: heightOfCanvas,
+                decoration: BoxDecoration(color: Colors.greenAccent),
+                child: Stack(
+                  children: [
+                    ...activeCircles.map(
+                      (circle) => GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            circle.color = Colors.blueAccent;
+                          });
+                          final snackbar = SnackBar(
+                            content: StatefulBuilder(
+                              builder:
+                                  (context, localSetState) => Slider.adaptive(
+                                    value: circle.radius,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        localSetState(() {
+                                          circle.radius = value;
+                                        });
+                                      });
+                                    },
+                                    onChangeEnd: (value) => updateStack(),
+                                    min:
+                                        widthOfScreen < heightOfScreen
+                                            ? widthOfScreen / 50
+                                            : heightOfScreen / 50,
+                                    max: 150,
+                                  ),
+                            ),
+                            showCloseIcon: true,
+                          );
+
+                          ScaffoldMessenger.of(
+                            context,
+                          ).showSnackBar(snackbar).closed.then((_) {
+                            setState(() {
+                              circle.color = Colors.grey;
+                            });
+                          });
+                        },
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: circle.x * widthOfCanvas - circle.radius,
+                              top: circle.y * heightOfCanvas - circle.radius,
+                              child: Container(
+                                width: circle.radius * 2,
+                                height: circle.radius * 2,
+                                decoration: BoxDecoration(
+                                  color: circle.color,
+                                  border: Border.all(
+                                    color: Colors.yellowAccent,
+                                  ),
+                                  shape: BoxShape.circle,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Circle {
+  double x;
+  double y;
+  double radius;
+  bool showDialog;
+  Color color;
+
+  Circle({
+    required this.x,
+    required this.y,
+    required this.radius,
+    required this.showDialog,
+    required this.color,
+  });
+
+  Circle copy() =>
+      Circle(x: x, y: y, radius: radius, showDialog: showDialog, color: color);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Circle &&
+        other.x == x &&
+        other.y == y &&
+        other.radius == radius &&
+        other.showDialog == showDialog &&
+        other.color == color;
+  }
+
+  @override
+  int get hashCode =>
+      x.hashCode ^
+      y.hashCode ^
+      radius.hashCode ^
+      showDialog.hashCode ^
+      color.hashCode;
 }
---
+++ circle/lib/stack.dart
@@ -0,0 +1,16 @@
+class StackDs<E> {
+  final _list = <E>[];
+
+  void push(E value) => _list.add(value);
+  E pop() => _list.removeLast();
+
+  E get peek => _list.last;
+
+  bool get isEmpty => _list.isEmpty;
+  int get length => _list.length;
+
+  @override
+  String toString() {
+    return _list.toString();
+  }
+}
